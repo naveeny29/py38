ea838160783b8e89d99e2db59cdcb3b1448fae48
ravi-kiran-yallapu <ravikiranyallapu@gmail.com>
2024-07-12 23:57:44 +0530

    demo.sh

--- demo.sh
+++ demo.sh
@@ -3,9 +3,18 @@ Set -e
 echo "You have passed $# Arguments"
 #This Scrip Will Get All The VPC ID Information From The Requested Region.
 if [ $# -gt 0 ]
-aws ec2 describe-vpcs --region $1|jq ".Vpcs[].VpcId"-r
+aws ec2 describe-vpcs --region $1|jq ".Vpcs[].VpcId" -r
 echo "======================================================="
-aws ec2 describe-vpcs --region $2|jq ".Vpcs[].VpcId"-r
+aws ec2 describe-vpcs --region $2|jq ".Vpcs[].VpcId" -r
+echo "======================================================="
+else
+echo "You have not given any arguments. You need to pass atleast two positional Arguments"
+fi
+if test $# -gt 0
+then
+aws ec2 describe-vpcs --region $1|jq ".Vpcs[].VpcId" -r
+echo "======================================================="
+aws ec2 describe-vpcs --region $2|jq ".Vpcs[].VpcId" -r
 echo "======================================================="
 else
 echo "You have not given any arguments. You need to pass atleast two positional Arguments"
