e2c73b5ceff44afc3a7af9c8be9696f9e63e943c
ravi-kiran-yallapu <ravikiranyallapu@gmail.com>
2024-07-13 01:37:21 +0530

    spl_args.sh

--- special_args.sh
+++ special_args.sh
@@ -3,6 +3,7 @@ if [ $# -gt 0 ];then
     for regions in $Regions
     do
     aws ec2 describe-vpcs --region $regions | jq ".Vpcs[].VpcId" -r 
+    echo "==================================================="
     done
 else
     echo "invalid code"
